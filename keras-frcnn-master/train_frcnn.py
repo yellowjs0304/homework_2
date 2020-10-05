719348de6b7f90499329339741a02c21b345de70
Jisu.H <30281608+yellowjs0304@users.noreply.github.com>
2020-10-05 11:58:06 +0900

    Update train_frcnn.py

--- keras-frcnn-master/train_frcnn.py
+++ keras-frcnn-master/train_frcnn.py
@@ -23,7 +23,7 @@ parser = OptionParser()
 
 parser.add_option("-p", "--path", dest="train_path", help="Path to training data.")
 parser.add_option("-o", "--parser", dest="parser", help="Parser to use. One of simple or pascal_voc",
-				default="pascal_voc")
+				default="simple")
 parser.add_option("-n", "--num_rois", dest="num_rois", help="Number of RoIs to process at once. ROI값 조절 Default값은 32었음.", default=20)
 parser.add_option("--network", dest="network", help="Base network to use. Supports vgg or resnet50.", default='resnet50')
 parser.add_option("--hf", dest="horizontal_flips", help="Augment with horizontal flips in training. (Default=false).", action="store_true", default=False)
